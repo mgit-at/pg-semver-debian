e706759ae03635b3d7e361c69d1bb2f42105e47e
Christian Pointner <cpointner@mgit.at>
2016-05-17 14:46:36 +0200

    imported pg-semver 0.10.0

---
+++ sql/semver.sql
@@ -0,0 +1,254 @@
+SET client_min_messages TO warning;
+SET log_min_messages    TO warning;
+
+-- Create a semantic version data type.
+--
+-- http://semver.org/.
+--
+
+-- 1. A normal version number MUST take the form X.Y.Z where X, Y, and Z are
+-- integers. X is the major version, Y is the minor version, and Z is the
+-- patch version. Each element MUST increase numerically. For instance: 1.9.0
+-- < 1.10.0 < 1.11.0.
+--
+-- 2. A special version number MAY be denoted by appending an arbitrary string
+-- immediately following the patch version. The string MUST be comprised of
+-- only alphanumerics plus dash [0-9A-Za-z-] and MUST begin with an alpha
+-- character [A-Za-z]. Special versions satisfy but have a lower precedence
+-- than the associated normal version. Precedence SHOULD be determined by
+-- lexicographic ASCII sort order. For instance: 1.0.0beta1 < 1.0.0beta2 <
+-- 1.0.0.
+
+CREATE TYPE semver;
+
+--
+-- essential IO
+--
+CREATE OR REPLACE FUNCTION semver_in(cstring)
+	RETURNS semver
+	AS 'semver'
+	LANGUAGE C STRICT IMMUTABLE;
+
+CREATE OR REPLACE FUNCTION semver_out(semver)
+	RETURNS cstring
+	AS 'semver'
+	LANGUAGE C STRICT IMMUTABLE;
+
+--
+--  The type itself.
+--
+
+CREATE TYPE semver (
+	INPUT = semver_in,
+	OUTPUT = semver_out,
+-- values of passedbyvalue and alignment are copied from the named type.
+    STORAGE = plain,
+	INTERNALLENGTH = variable,
+-- string category, to automatically try string conversion, etc.
+	CATEGORY = 'S',
+	PREFERRED = false
+);
+
+--
+--  A lax constructor function.
+--
+
+CREATE OR REPLACE FUNCTION to_semver(text)
+	RETURNS semver
+	AS 'semver'
+	LANGUAGE C STRICT IMMUTABLE;
+
+--
+--  Typecasting functions.
+--
+
+CREATE OR REPLACE FUNCTION semver(text)
+	RETURNS semver
+    AS 'semver', 'text_to_semver'
+	LANGUAGE C STRICT IMMUTABLE;
+
+CREATE OR REPLACE FUNCTION text(semver)
+	RETURNS text
+    AS 'semver', 'semver_to_text'
+	LANGUAGE C STRICT IMMUTABLE;
+
+CREATE OR REPLACE FUNCTION semver(numeric)
+	RETURNS semver AS $$ SELECT to_semver($1::text) $$
+    LANGUAGE SQL STRICT IMMUTABLE;
+
+CREATE OR REPLACE FUNCTION semver(real)
+	RETURNS semver AS $$ SELECT to_semver($1::text) $$
+    LANGUAGE SQL STRICT IMMUTABLE;
+
+CREATE OR REPLACE FUNCTION semver(double precision)
+	RETURNS semver AS $$ SELECT to_semver($1::text) $$
+    LANGUAGE SQL STRICT IMMUTABLE;
+
+CREATE OR REPLACE FUNCTION semver(integer)
+	RETURNS semver AS $$ SELECT to_semver($1::text) $$
+    LANGUAGE SQL STRICT IMMUTABLE;
+
+CREATE OR REPLACE FUNCTION semver(smallint)
+	RETURNS semver AS $$ SELECT to_semver($1::text) $$
+    LANGUAGE SQL STRICT IMMUTABLE;
+
+CREATE OR REPLACE FUNCTION semver(bigint)
+	RETURNS semver AS $$ SELECT to_semver($1::text) $$
+    LANGUAGE SQL STRICT IMMUTABLE;
+
+--
+--  Explicit type casts.
+--
+
+CREATE CAST (semver AS text)             WITH FUNCTION text(semver);
+CREATE CAST (text AS semver)             WITH FUNCTION semver(text);
+CREATE CAST (numeric AS semver)          WITH FUNCTION semver(numeric);
+CREATE CAST (real AS semver)             WITH FUNCTION semver(real);
+CREATE CAST (double precision AS semver) WITH FUNCTION semver(double precision);
+CREATE CAST (integer AS semver)          WITH FUNCTION semver(integer);
+CREATE CAST (smallint AS semver)         WITH FUNCTION semver(smallint);
+CREATE CAST (bigint AS semver)           WITH FUNCTION semver(bigint);
+
+--
+--	Comparison functions and their corresponding operators.
+--
+
+CREATE OR REPLACE FUNCTION semver_eq(semver, semver)
+	RETURNS bool
+	AS 'semver'
+	LANGUAGE C STRICT IMMUTABLE;
+
+CREATE OPERATOR = (
+	leftarg = semver,
+	rightarg = semver,
+	negator = <>,
+	procedure = semver_eq,
+	restrict = eqsel,
+	commutator = =,
+	join = eqjoinsel,
+	hashes, merges
+);
+
+CREATE OR REPLACE FUNCTION semver_ne(semver, semver)
+	RETURNS bool
+	AS 'semver'
+	LANGUAGE C STRICT IMMUTABLE;
+
+CREATE OPERATOR <> (
+	leftarg = semver,
+	rightarg = semver,
+	negator = =,
+	procedure = semver_ne,
+	restrict = neqsel,
+	join = neqjoinsel
+);
+
+CREATE OR REPLACE FUNCTION semver_le(semver, semver)
+	RETURNS bool
+	AS 'semver'
+	LANGUAGE C STRICT IMMUTABLE;
+
+CREATE OPERATOR <= (
+	leftarg = semver,
+	rightarg = semver,
+	negator = >,
+	procedure = semver_le
+);
+
+CREATE OR REPLACE FUNCTION semver_lt(semver, semver)
+	RETURNS bool
+	AS 'semver'
+	LANGUAGE C STRICT IMMUTABLE;
+
+CREATE OPERATOR < (
+	leftarg = semver,
+	rightarg = semver,
+	negator = >=,
+	procedure = semver_lt
+);
+
+CREATE OR REPLACE FUNCTION semver_ge(semver, semver)
+	RETURNS bool
+	AS 'semver'
+	LANGUAGE C STRICT IMMUTABLE;
+
+CREATE OPERATOR >= (
+	leftarg = semver,
+	rightarg = semver,
+	negator = <,
+	procedure = semver_ge
+);
+
+CREATE OR REPLACE FUNCTION semver_gt(semver, semver)
+	RETURNS bool
+	AS 'semver'
+	LANGUAGE C STRICT IMMUTABLE;
+
+CREATE OPERATOR > (
+	leftarg = semver,
+	rightarg = semver,
+	negator = <=,
+	procedure = semver_gt
+);
+
+--
+-- Support functions for indexing.
+--
+
+CREATE OR REPLACE FUNCTION semver_cmp(semver, semver)
+	RETURNS int4
+	AS 'semver'
+	LANGUAGE C STRICT IMMUTABLE;
+
+CREATE OR REPLACE FUNCTION hash_semver(semver)
+	RETURNS int4
+	AS 'semver'
+	LANGUAGE C STRICT IMMUTABLE;
+
+--
+-- The btree indexing operator class.
+--
+
+CREATE OPERATOR CLASS semver_ops
+DEFAULT FOR TYPE SEMVER USING btree AS
+    OPERATOR    1   <  (semver, semver),
+    OPERATOR    2   <= (semver, semver),
+    OPERATOR    3   =  (semver, semver),
+    OPERATOR    4   >= (semver, semver),
+    OPERATOR    5   >  (semver, semver),
+    FUNCTION    1   semver_cmp(semver, semver);
+
+--
+-- The hash indexing operator class.
+--
+
+CREATE OPERATOR CLASS semver_ops
+DEFAULT FOR TYPE semver USING hash AS
+    OPERATOR    1   =  (semver, semver),
+    FUNCTION    1   hash_semver(semver);
+
+--
+-- Aggregates.
+--
+
+CREATE OR REPLACE FUNCTION semver_smaller(semver, semver)
+	RETURNS semver
+	AS 'semver'
+	LANGUAGE C STRICT IMMUTABLE;
+
+CREATE AGGREGATE min(semver)  (
+    SFUNC = semver_smaller,
+    STYPE = semver,
+    SORTOP = <
+);
+
+CREATE OR REPLACE FUNCTION semver_larger(semver, semver)
+	RETURNS semver
+	AS 'semver'
+	LANGUAGE C STRICT IMMUTABLE;
+
+CREATE AGGREGATE max(semver)  (
+    SFUNC = semver_larger,
+    STYPE = semver,
+    SORTOP = >
+);
---
+++ test/sql/base.sql
@@ -0,0 +1,306 @@
+\set ECHO none
+BEGIN;
+
+\i test/pgtap-core.sql
+\i sql/semver.sql
+
+-- Before 8.4, there was no unnest(), so create one.
+CREATE FUNCTION create_unnest(
+) RETURNS SETOF BOOLEAN LANGUAGE PLPGSQL AS $$
+BEGIN
+    IF pg_version_num() < 80400 THEN
+        EXECUTE $F$ CREATE FUNCTION unnest(
+            anyarray
+        ) RETURNS SETOF anyelement LANGUAGE sql AS $_$
+            SELECT $1[i]
+              FROM generate_series(array_lower($1, 1), array_upper($1, 1)) AS i;
+        $_$;$F$;
+    END IF;
+END;
+$$;
+
+SELECT * FROM create_unnest();
+
+SELECT plan(225);
+--SELECT * FROM no_plan();
+
+SELECT has_type('semver');
+SELECT is( NULL::semver, NULL, 'semvers should be NULLable' );
+
+SELECT lives_ok(
+    $$ SELECT '$$ || v || $$'::semver $$,
+    '"' || v || '" is a valid semver'
+)  FROM unnest(ARRAY[
+    '1.2.2',
+    '0.2.2',
+    '0.0.0',
+    '0.1.999',
+    '9999.9999999.823823',
+    '1.0.0-beta1',
+    '1.0.0-beta2',
+    '1.0.0',
+    '1.0.0-1',
+    '1.0.0-alpha+d34dm34t',
+    '1.0.0+d34dm34t',
+    '20110204.0.0'
+]) AS v;
+
+SELECT throws_ok(
+    $$ SELECT '$$ || v || $$'::semver $$,
+    NULL,
+    '"' || v || '" is not a valid semver'
+)  FROM unnest(ARRAY[
+   '1.2',
+   '1.2.02',
+   '1.2.2-',
+   '1.2.3b#5',
+   '03.3.3',
+   'v1.2.2',
+   '1.3b',
+   '1.4b.0',
+   '1v',
+   '1v.2.2v',
+   '1.2.4b.5'
+]) AS v;
+
+-- Test =, <=, and >=.
+SELECT collect_tap(ARRAY[
+    ok(semver_cmp(lv::semver, rv::semver) = 0, 'semver(' || lv || ', ' || rv || ') should = 0'),
+    ok(lv::semver = rv::semver, 'v' || lv || ' should = v' || rv),
+    ok(lv::semver <= rv::semver, 'v' || lv || ' should be <= v' || rv),
+    ok(lv::semver >= rv::semver, 'v' || lv || ' should be >= v' || rv)
+]) FROM (VALUES
+    ('1.2.2',  '1.2.2'),
+    ('1.2.23', '1.2.23'),
+    ('0.0.0', '0.0.0'),
+    ('999.888.7777', '999.888.7777'),
+    ('0.1.2-beta3', '0.1.2-beta3'),
+    ('1.0.0-rc-1', '1.0.0-RC-1')
+ ) AS f(lv, rv);
+
+-- Test semver <> semver
+SELECT collect_tap(ARRAY[
+    ok(semver_cmp(lv::semver, rv::semver) <> 0, 'semver(' || lv || ', ' || rv || ') should <> 0'),
+    ok(lv::semver <> rv::semver, 'v' || lv || ' should not equal v' || rv)
+]) FROM (VALUES
+    ('1.2.2', '1.2.3'),
+    ('0.0.1', '1.0.0'),
+    ('1.0.1', '1.1.0'),
+    ('1.1.1', '1.1.0'),
+    ('1.2.3-b', '1.2.3'),
+    ('1.2.3', '1.2.3-b'),
+    ('1.2.3-a', '1.2.3-b'),
+    ('1.2.3-aaaaaaa1', '1.2.3-aaaaaaa2'),
+    ('1.2.3-1.2.3', '1.2.3-1.2.3.4')
+  ) AS f(lv, rv);
+
+-- Test >, >=, <, and <=.
+SELECT collect_tap(ARRAY[
+    ok( semver_cmp(lv::semver, rv::semver) > 0, 'semver(' || lv || ', ' || rv || ') should > 0'),
+    ok( semver_cmp(rv::semver, lv::semver) < 0, 'semver(' || rv || ', ' || lv || ') should < 0'),
+    ok(lv::semver > rv::semver, 'v' || lv || ' should be > v' || rv),
+    ok(lv::semver >= rv::semver, 'v' || lv || ' should be >= v' || rv),
+    ok(rv::semver < lv::semver, 'v' || rv || ' should be < v' || lv),
+    ok(rv::semver <= lv::semver, 'v' || rv || ' should be <= v' || lv)
+]) FROM (VALUES
+    ('2.2.2', '1.1.1'),
+    ('2.2.2', '2.1.1'),
+    ('2.2.2', '2.2.1'),
+    ('2.2.2-b', '2.2.1'),
+    ('2.2.2', '2.2.2-b'),
+    ('2.2.2-c', '2.2.2-b'),
+    ('2.2.2-rc-2', '2.2.2-RC-1'),
+    ('0.9.10', '0.9.9'),
+    ('1.0.1-1.2.3', '1.0.1-0.9.9.9')
+  ) AS f(lv, rv);
+
+-- Test to_semver().
+SELECT has_function('to_semver');
+SELECT has_function('to_semver', ARRAY['text']);
+SELECT function_returns('to_semver', 'semver');
+
+SELECT is(
+    to_semver(dirty),
+    clean::semver,
+    'to_semver(' || dirty || ') should return ' || clean
+) FROM (VALUES
+    ('1.2.2',           '1.2.2'),
+    ('01.2.2',          '1.2.2'),
+    ('1.02.2',          '1.2.2'),
+    ('1.2.02',          '1.2.2'),
+    ('1.2.02b',        '1.2.2-b'),
+    ('1.2.02beta-3  ', '1.2.2-beta-3'),
+    ('1.02.02rc1',     '1.2.2-rc1'),
+    ('1.0',             '1.0.0'),
+    ('1',               '1.0.0'),
+    ('.0.02',           '0.0.2'),
+    ('1..02',           '1.0.2'),
+    ('1..',             '1.0.0'),
+    ('1.1',             '1.1.0'),
+    ('1.2.b1',          '1.2.0-b1'),
+    ('9.0beta4',        '9.0.0-beta4'), -- PostgreSQL format.
+    ('9b',              '9.0.0-b'),
+    ('rc1',             '0.0.0-rc1'),
+    ('',                '0.0.0'),
+    ('..2',             '0.0.2'),
+    ('1.2.3 a',         '1.2.3-a'),
+    ('..2 b',           '0.0.2-b'),
+    ('  012.2.2',       '12.2.2'),
+    ('20110204',        '20110204.0.0')
+) v(dirty, clean);
+
+-- to_semver still needs to reject truly bad input
+SELECT throws_ok(
+    $$ SELECT '$$ || v || $$'::semver $$,
+    NULL,
+    '"' || v || '" is not a valid semver'
+)  FROM unnest(ARRAY[
+   '1.2.0 beta 4',
+   '1.2.2-',
+   '1.2.3b#5',
+   'v1.2.2',
+   '1.4b.0',
+   '1v.2.2v',
+   '1.2.4b.5',
+   '1.2.3.4',
+   '1.2.3 4',
+   '1.2000000000000000.3.4'
+]) AS v;
+
+-- Test sort ordering
+CREATE TABLE vs (
+    version semver
+);
+
+INSERT INTO vs VALUES ('1.2.0'), ('1.0.0'), ('1.0.0-p0'), ('0.9.9'), ('0.9.10');
+
+SELECT is(max(version), '1.2.0', 'max(semver) should work')
+  FROM vs;
+
+SELECT is(min(version), '0.9.9', 'min(semver) should work')
+  FROM vs;
+
+SELECT results_eq(
+    $$ SELECT version FROM vs ORDER BY version USING < $$,
+    $$ VALUES ('0.9.9'::semver), ('0.9.10'::semver), ('1.0.0-p0'::semver), ('1.0.0'::semver), ('1.2.0'::semver) $$,
+    'ORDER BY semver USING < should work'
+);
+
+SELECT results_eq(
+    $$ SELECT version FROM vs ORDER BY version USING > $$,
+    $$ VALUES ('1.2.0'::semver), ('1.0.0'::semver), ('1.0.0-p0'::semver), ('0.9.10'::semver), ('0.9.9'::semver) $$,
+    'ORDER BY semver USING > should work'
+);
+
+-- Test constructors.
+SELECT is( text('1.2.0'::semver), '1.2.0', 'construct to text' );
+SELECT is( semver('1.2.0'), '1.2.0'::semver, 'construct from text' );
+SELECT is( semver(1.2), '1.2.0'::semver, 'construct from bare number' );
+SELECT is( semver(1.2::numeric), '1.2.0'::semver, 'construct from numeric' );
+SELECT is( semver(1), '1.0.0'::semver, 'construct from bare integer' );
+SELECT is( semver(1::integer), '1.0.0'::semver, 'construct from integer' );
+SELECT is( semver(1::bigint), '1.0.0'::semver, 'construct from bigint' );
+SELECT is( semver(1::smallint), '1.0.0'::semver, 'construct from smallint' );
+SELECT is( semver(1.2::decimal), '1.2.0'::semver, 'construct from decimal' );
+SELECT is( semver(1.2::real), '1.2.0'::semver, 'construct from real' );
+SELECT is( semver(1.2::double precision), '1.2.0'::semver, 'construct from double' );
+SELECT is( semver(1.2::float), '1.2.0'::semver, 'construct from float' );
+
+-- Test casting.
+SELECT is( semver('1.2.0'::text), '1.2.0', 'cast to text' );
+SELECT is( text('1.2.0')::semver, '1.2.0'::semver, 'cast from text' );
+SELECT is( 1::semver, '1.0.0'::semver, 'Cast from bare integer');
+SELECT is( 1.2::semver, '1.2.0'::semver, 'Cast from bare number');
+SELECT is( 1.2::numeric::semver, '1.2.0'::semver, 'Cast from numeric');
+SELECT is( 1::integer::semver, '1.0.0'::semver, 'Cast from integer');
+SELECT is( 1::bigint::semver, '1.0.0'::semver, 'Cast from bigint');
+SELECT is( 1::smallint::semver, '1.0.0'::semver, 'Cast from smallint');
+SELECT is( 1.0::decimal::semver, '1.0.0'::semver, 'Cast from decimal');
+SELECT is( 1::decimal::semver, '1.0.0'::semver, 'Cast from decimal');
+SELECT is( 1.0::real::semver, '1.0.0'::semver, 'Cast from real');
+SELECT is( 1.0::double precision::semver, '1.0.0'::semver, 'Cast from double precision');
+SELECT is( 1.0::float::semver, '1.0.0'::semver, 'Cast from float');
+
+-- Test casting some more.
+SELECT IS(lv::text, rv, 'Should correctly cast "' || rv || '" to text')
+  FROM (VALUES
+    ('1.0.0-beta'::semver,   '1.0.0-beta'),
+    ('1.0.0-beta1'::semver,  '1.0.0-beta1'),
+    ('1.0.0-alpha'::semver,  '1.0.0-alpha'),
+    ('1.0.0-alph'::semver,   '1.0.0-alph'),
+    ('1.0.0-food'::semver,   '1.0.0-food'),
+    ('1.0.0-f111'::semver,   '1.0.0-f111'),
+    ('1.0.0-f111asbcdasdfasdfasdfasdfasdfasdffasdfadsf'::semver,
+     '1.0.0-f111asbcdasdfasdfasdfasdfasdfasdffasdfadsf')
+ ) AS f(lv, rv);
+
+-- SEMV 2.0.0 tests.
+SELECT lives_ok(
+    $$ SELECT '$$ || v || $$'::semver $$,
+    '"' || v || '" is a valid 2.0.0 semver'
+)  FROM unnest(ARRAY[
+    '1.0.0+1',
+    '1.0.0-1+1',
+    '1.0.0-1.1+1',
+    '1.0.0-1.1.1.1.1.1.1.1.1.1.1+1.1.1.1.1.1.1.1',
+    '1.0.0-1.2',
+    '1.0.0-1.0.2',
+    '1.0.0-alpha',
+    '1.0.0-alpha.1',
+    '1.0.0-0.3.7',
+    '1.0.0-x.7.z.92'
+]) AS v;
+
+SELECT throws_ok(
+    $$ SELECT '$$ || v || $$'::semver $$,
+    NULL,
+    '"' || v || '" is not a valid 2.0.0 semver'
+)  FROM unnest(ARRAY[
+   '1.0.0-a..',
+   '1.0.0-a.1.',
+   '1.0.0+1-1',
+   '1.0.0-1....',
+   '1.0.0-1_2',
+   '1.0.0-1.02'
+]) AS v;
+
+DELETE FROM vs;
+INSERT INTO vs VALUES ('0.9.9-a1.1+1234'::semver), ('0.9.9-a1.2.3'::semver), ('0.9.9-a1.2'::semver), ('0.9.9'::semver), ('1.0.0+99'::semver), ('1.0.0-1'::semver);
+
+SELECT results_eq(
+    $$ SELECT version FROM vs ORDER BY version USING < $$,
+    $$ VALUES ('0.9.9-a1.1+1234'::semver), ('0.9.9-a1.2'::semver), ('0.9.9-a1.2.3'::semver), ('0.9.9'::semver), ('1.0.0+99'::semver), ('1.0.0-1'::semver) $$,
+    'ORDER BY semver (2.0.0) USING < should work'
+);
+
+SELECT results_eq(
+    $$ SELECT version FROM vs ORDER BY version USING > $$,
+    $$ VALUES ('1.0.0-1'::semver), ('1.0.0+99'::semver), ('0.9.9'::semver), ('0.9.9-a1.2.3'::semver), ('0.9.9-a1.2'::semver), ('0.9.9-a1.1+1234'::semver) $$,
+    'ORDER BY semver (2.0.0) USING > should work'
+);
+
+SELECT collect_tap(ARRAY[
+    ok(semver_cmp(lv::semver, rv::semver) = 0, 'semver(' || lv || ', ' || rv || ') should = 0'),
+    ok(lv::semver = rv::semver, 'v' || lv || ' should = v' || rv),
+    ok(lv::semver <= rv::semver, 'v' || lv || ' should be <= v' || rv),
+    ok(lv::semver >= rv::semver, 'v' || lv || ' should be >= v' || rv)
+]) FROM (VALUES
+    ('1.0.0-1+1',  '1.0.0-1+5'),
+    ('1.0.0-1.1+1',  '1.0.0-1.1+5')
+ ) AS f(lv, rv);
+
+-- Regressions.
+SELECT is(
+    to_semver(lv)::TEXT, rv,
+    'Should correctly represent "' || lv || '" as "' || rv || '"'
+) FROM (VALUES
+      ('0.5.0-release1', '0.5.0-release1'),
+      ('0.5.0release1',  '0.5.0-release1'),
+      ('0.5-release1',   '0.5.0-release1'),
+      ('0.5release1',    '0.5.0-release1'),
+      ('0.5-1',          '0.5.0-1'),
+      ('1.2.3-1.02',     '1.2.3-1.2')
+) AS f(lv, rv);
+
+SELECT * FROM finish();
+ROLLBACK;
